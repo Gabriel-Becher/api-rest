3eafe426210dafab915f002afba2be12e088211d
GabrielBecher <gabriel.abecher@hotmail.com>
2024-09-12 18:02:36 -0300

    prod

--- server.js
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-import dotenv from "dotenv";
-dotenv.config();
-
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Server rodando na porta ${port}`);
-  console.log(`http://localhost:${port}`);
-});
